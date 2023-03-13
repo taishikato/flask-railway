127db92890a12b3d0eba5d86b1c2be5e969b5bfc
taishikato <taishi.k0903@gmail.com>
2023-03-13 01:47:54 -0700

    use small model

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-model = whisper.load_model("base")
 
 @app.route('/')
 def index():
@@ -27,6 +26,7 @@ def download():
     print(file.filename)
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    model = whisper.load_model("small")
     result = model.transcribe('./' + filename, verbose=True, language='en')
     print(result)
     os.remove('./' + filename)
