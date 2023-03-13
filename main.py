6812882e3ba3b6c2a1ad4ff765e56e4ccc1e5468
taishikato <taishi.k0903@gmail.com>
2023-03-13 01:19:30 -0700

    add /download route

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 from flask import Flask, jsonify
 import os
+from flask import Flask, render_template
+import json
+from flask import Flask,request
+import whisper
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/download", methods=['POST'])
+def download():
+  file = request.files['file']
+  print(file)
+
+  if file.filename != None:
+    print(file.filename)
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    model = whisper.load_model("base")
+    result = model.transcribe('./' + filename, verbose=True, language='en')
+    print(result)
+    os.remove('./' + filename)
+    model.cpu()
+    del model
+
+    # requests.post("http://localhost:3000/api/save-transcriptions-test", data=json.dumps(result))
+
+    return json.dumps(result)
+
+  return 'hello'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
