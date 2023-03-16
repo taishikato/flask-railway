906230a17ea0b87ddcca92a4c299b01a413b23cd
taishikato <taishi.k0903@gmail.com>
2023-03-15 23:04:24 -0700

    change location

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 UPLOAD_FOLDER = './'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+model = whisper.load_model("small")
+
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
@@ -24,7 +26,6 @@ def download():
     print(file.filename)
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    model = whisper.load_model("small")
     result = model.transcribe('./' + filename, verbose=True, language='en')
     print(result)
     os.remove('./' + filename)
