ee49a86e0b7e2089ef6d467227fbf9ff88133a09
taishikato <taishi.k0903@gmail.com>
2023-03-19 15:29:30 -0700

    call save-document endpoint

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 from flask import Flask,request
 import whisper
 import os
+import requests
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -20,21 +21,30 @@ def index():
 @app.route("/download", methods=['POST'])
 def download():
   file = request.files['file']
-  print(file)
+  filterId = request.form.get('filterId')
+  userId = request.form.get('userId')
+
+  print('file', file)
+  print('filterId', filterId)
+  print('userId', userId)
 
   if file.filename != None:
     print(file.filename)
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     result = model.transcribe('./' + filename, verbose=True, language='en')
-    # print(result)
     os.remove('./' + filename)
     # model.cpu()
     # del model
 
-    # requests.post("http://localhost:3000/api/save-transcriptions-test", data=json.dumps(result))
+    requests.post("https://audioscript.vercel.app/api/save-document", data={
+        "filterId": filterId,
+        "userId": userId,
+        "transcriptions": json.dumps(result),
+        "fileName": file.filename
+    })
 
-    return json.dumps(result)
+    return 'done'
 
   return 'hello'
 
